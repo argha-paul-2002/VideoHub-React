7ff7f2e1c046b6e46b8cfa3bc1f8c3a8662626a2
Argha Paul <arghapaul002@gmail.com>
2023-07-27 17:22:52 +0530

    Header created

--- src/ColorModeSwitcher.js
+++ src/ColorModeSwitcher.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { useColorMode, useColorModeValue, IconButton } from '@chakra-ui/react';
 import { FaMoon, FaSun } from 'react-icons/fa';
 
-export const ColorModeSwitcher = props => {
+const ColorModeSwitcher = props => {
   const { toggleColorMode } = useColorMode();
   const text = useColorModeValue('dark', 'light');
   const SwitchIcon = useColorModeValue(FaMoon, FaSun);
@@ -10,9 +10,11 @@ export const ColorModeSwitcher = props => {
   return (
     <IconButton
       size="md"
-      fontSize="lg"
       aria-label={`Switch to ${text} mode`}
       variant="ghost"
+      pos={"fixed"}
+      top={'4'}
+      right={'4'}
       color="current"
       marginLeft="2"
       onClick={toggleColorMode}
@@ -21,3 +23,5 @@ export const ColorModeSwitcher = props => {
     />
   );
 };
+
+export default ColorModeSwitcher;
